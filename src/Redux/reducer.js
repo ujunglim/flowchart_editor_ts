7b0849c5b34615231f69a131054311a93b5528c3
Ziwang_Li <ujunglim@naver.com>
2021-08-24 14:38:55 +0800

    Revert "createReducer"
    This reverts commit 381a8c28a34d69b4cc9f89ae2bb0e4d154a26ba0.

--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -1,40 +1,4 @@
-// import { createAction } from "@reduxjs/toolkit";
-// import { createStore } from "redux";
-
-// const initialState = {
-// 	routeNum: 2,
-// 	setRelation: false
-// };
-
-// // Action creators
-// const plus = createAction("PLUS");
-// const minus = createAction("MINUS");
-// const relation = createAction("SET_RELATION")
-
-// // reducer
-// const reducer = (state = initialState, action) => {
-// 	switch(action.type) {
-// 		case plus.type:
-// 			// console.log('+');
-// 			return {routeNum: state.routeNum + 1, action: "plus", setRelation: true}
-// 		case minus.type:
-// 			// console.log('-')
-// 			return {routeNum: state.routeNum - 1, action: "minus", setRelation: true}
-// 		case relation.type:
-// 			return {routeNum: state.routeNum, setRelation: action.payload}
-// 		default:
-// 			return state;
-// 	}
-// }
-
-// // create store
-// const store = createStore(reducer);
-// export default store;
-
-
-//=============================================================
-
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { createAction } from "@reduxjs/toolkit";
 import { createStore } from "redux";
 
 const initialState = {
@@ -45,62 +9,23 @@ const initialState = {
 // Action creators
 const plus = createAction("PLUS");
 const minus = createAction("MINUS");
-const setRelation = createAction("SET_RELATION")
+const relation = createAction("SET_RELATION")
 
-// // reducer
-// const reducer = (state = initialState, action) => {
-// 	switch(action.type) {
-// 		case plus.type:
-// 			// console.log('+');
-// 			return {routeNum: state.routeNum + 1, action: "plus", setRelation: true}
-// 		case minus.type:
-// 			// console.log('-')
-// 			return {routeNum: state.routeNum - 1, action: "minus", setRelation: true}
-// 		case setRelation.type:
-// 			return {routeNum: state.routeNum, setRelation: action.payload}
-// 		default:
-// 			return state;
-// 	}
-// }
-
-
-// reducer with builder callback object notation
-const reducer = createReducer(initialState, (builder) => {
-	builder
-		.addCase(plus, (state, action) => {
-			state.routeNum++;
-			state.action = "plus";
-			state.setRelation = true;
-		})
-		.addCase(minus, (state, action) => {
-			state.routeNum--;
-			state.action = "minus";
-			state.setRelation = true;
-		})
-		.addCase(setRelation, (state, {payload}) => {
-			state.setRelation = payload;
-		})
-})
-
-
-// // reducer with map object notation
-// const reducer = createReducer(initialState, {
-// 	[plus]: (state, action) => {
-// 		state.routeNum++;
-// 		state.action = "plus";
-// 		state.setRelation = true;
-// 	},
-// 	[minus]: (state, action) => {
-// 		state.routeNum--;
-// 		state.action = "minus";
-// 		state.setRelation = true;
-// 	},
-// 	[setRelation] : (state, {payload}) => {
-// 		state.setRelation = payload;
-// 	}
-// })
-
-
+// reducer
+const reducer = (state = initialState, action) => {
+	switch(action.type) {
+		case plus.type:
+			// console.log('+');
+			return {routeNum: state.routeNum + 1, action: "plus", setRelation: true}
+		case minus.type:
+			// console.log('-')
+			return {routeNum: state.routeNum - 1, action: "minus", setRelation: true}
+		case relation.type:
+			return {routeNum: state.routeNum, setRelation: action.payload}
+		default:
+			return state;
+	}
+}
 
 // create store
 const store = createStore(reducer);
